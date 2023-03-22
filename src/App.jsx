fff9c59cfdaa5cc5c69e63c0f959532444180ab3
Owahed Hossain <owahedhossain@gmail.com>
2023-03-22 12:27:23 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/navbar";
+import "./index.css";
+import Hero from "./page/Hero/Hero";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import "./navbar.scss";
+
+const Navbar = () => {
+  const [value, setValue] = useState(false);
+  console.log(value);
+  return (
+    <div className="navbar-section elementor-container">
+      <div className={value ? "navbar-content  height" : "navbar-content  "}>
+        <div>
+          <span>CARRO</span>
+        </div>
+        <div onClick={() => setValue(!value)} className="nav-toggle ">
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+
+        <div className={value ? "navbar-name" : "navbar-name active"}>
+          <a href="/">Home</a>
+          <a href="#">About</a>
+          <a href="/">Vehicle Models</a>
+          <a href="#">Testimonials</a>
+          <a href="/"> Our team</a>
+          <a href="#">Contact</a>
+        </div>
+
+        <div className={value ? "login-bar" : "login-bar active"}>
+          <a href="#">Sign in </a>
+          <a href="#">Register</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
