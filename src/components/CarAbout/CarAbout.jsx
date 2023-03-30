97c487f3b3342a9ef697eaec5db5b9789901c4ff
Owahed Hossain <owahedhossain@gmail.com>
2023-03-30 14:23:31 +0600

    terminal

--- src/components/CarAbout/CarAbout.jsx
+++ src/components/CarAbout/CarAbout.jsx
@@ -22,7 +22,7 @@ const CarAbout = () => {
           <h1>You start the engine and your adventure begins</h1>
           <p>
             Certain but she but shyness why cottage. Guy the put instrument sir
-            entreaties affronting. Pretended exquisite see cordially the you.
+            entreaties affronting. Pretended exquisite is see cordially the you.
             Weeks quiet do vexed or whose. Motionless if no to affronting
             imprudence no precaution. My indulged as disposal strongly attended.
           </p>
