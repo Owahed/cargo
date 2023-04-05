8dbb142b325046a4c7d975134a4855f110bdc28f
Owahed Hossain <owahedhossain@gmail.com>
2023-04-05 21:20:10 +0600

    end--

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -11,12 +11,14 @@ const Team = () => {
               <div class="flip-card-inner">
                 <div class="flip-card-front">
                   <img
+                    loading="lazy"
                     src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
                     alt=""
                   />
                 </div>
                 <div class="flip-card-back">
                   <img
+                    loading="lazy"
                     src="https://media.giphy.com/media/l4KibWpBGWchSqCRy/giphy.gif"
                     alt=""
                   />
@@ -34,12 +36,14 @@ const Team = () => {
               <div class="flip-card-inner">
                 <div class="flip-card-front">
                   <img
+                    loading="lazy"
                     src="https://images.pexels.com/photos/3973335/pexels-photo-3973335.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
                     alt=""
                   />
                 </div>
                 <div class="flip-card-back">
                   <img
+                    loading="lazy"
                     src="https://media4.giphy.com/media/l3vR5Sghh85EBCnhC/giphy.gif?cid=ecf05e4786trkpqmnl0q080j0qcgt9dtxdvmr1x8fkvhjljd&rid=giphy.gif&ct=g"
                     alt=""
                   />
@@ -57,12 +61,14 @@ const Team = () => {
               <div class="flip-card-inner">
                 <div class="flip-card-front">
                   <img
+                    loading="lazy"
                     src="https://images.pexels.com/photos/718978/pexels-photo-718978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
                     alt=""
                   />
                 </div>
                 <div class="flip-card-back">
                   <img
+                    loading="lazy"
                     src="https://media4.giphy.com/media/3ogmaPGsQOruw/200w.webp?cid=ecf05e47vrhrj4vupwhwn77ymnclehhrb0q3kd7unplgvcfx&rid=200w.webp&ct=g"
                     alt=""
                   />
